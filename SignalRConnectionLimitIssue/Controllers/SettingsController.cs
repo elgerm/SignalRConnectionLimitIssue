2e9e7dd90432de23597c871ebbd9783dd5fca8ac
jalalx <sjamini@gmail.com>
2017-02-02 12:34:28 +0330

    signalr support added

---
+++ SignalRConnectionLimitIssue/Controllers/SettingsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNet.SignalR;
+using SignalRConnectionLimitIssue.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace SignalRConnectionLimitIssue.Controllers
+{
+    [RoutePrefix("api/settings")]
+    public class SettingsController : ApiController
+    {
+        [Route("canConnect")]
+        [HttpGet]
+        public IHttpActionResult CanConnect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SignalRConnectionLimitIssue/Hubs/MessagingHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRConnectionLimitIssue.Hubs
+{
+    public class MessagingHub : Hub
+    {
+        [HubMethodName("sendOthers")]
+        public void SendToOthers(string message)
+        {
+            Clients.All.ReceiveMessage(message);
+        }
+    }
+}
